7615248e40b230525c733e5747f670c8558fd921
slin12 <s.lin12@gmail.com>
2017-11-18 15:06:23 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(number) {
+  return Math.abs(number - 42);
+}
+
+function distanceFromHqInFeet(number) {
+  return distanceFromHqInBlocks(number) * 264;
+}
+
+function distanceTravelledInFeet(start, end) {
+  return Math.abs(end - start) * 264;
+}
+
+function calculatesFarePrice(start, end) {
+  distance = distanceTravelledInFeet(start, end)
+  if (distance < 400) {
+    return 0;
+  }
+  else if (distance < 2000) {
+    return distance * 0.02;
+  }
+  else if (distance < 2500) {
+    return 25;
+  }
+  else {
+    return 'cannot travel that far';
+  }
+}
